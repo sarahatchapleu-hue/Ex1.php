7c5b1507292bbd4f6f6e75de0d6d0e8619680847
sarahatchapleu-hue <sarahatchapleu@gmail.com>
2025-11-17 13:27:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -15,4 +15,27 @@ function school ($age) {
     }
 }
 echo school (20);
+
+for ($i = 1; $i <= 100; $i++) {
+    if ($i % 15 == 0) {
+        echo "FooBar\n";
+    } elseif ($i % 3 == 0) {
+        echo "Foo\n";
+    } elseif ($i % 5 == 0) {
+        echo "Bar\n";
+    } else {
+        echo $i . "\n";
+    }
+
+}
+
+function doubleBoucle($n) {
+    for ($i = 1; $i <= $n; $i++) {
+        for ($j = 1; $j <= $i; $j++) {
+            echo $i;
+        }
+        echo "\n";
+    }
+}
+echo doubleBoucle(5);
 ?>
